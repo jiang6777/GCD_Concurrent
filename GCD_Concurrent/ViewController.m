731e34d21672559eb4f3ca07428cb09597bf112b
jiangshan <jiangshan.he>
2017-02-20 19:27:36 +0800

    增加了多线程死锁的情况

--- GCD_Concurrent/ViewController.m
+++ GCD_Concurrent/ViewController.m
@@ -54,6 +54,7 @@
      }
      */
     //第二种方式,使用dispatch_group_notify监听是否两个线程执行完了
+    /*
     dispatch_group_t group = dispatch_group_create();
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_group_enter(group);
@@ -74,8 +75,53 @@
     dispatch_group_notify(group, queue, ^{
         NSLog(@"---全部结束。。。");
     });
+     */
     
+    
+    /*
+     线程死锁：线程死锁主要是两个线程互相等待
+     造成死锁的几种方式
+     */
+    /* 第一种 1在等待2执行完再往下执行   2等待1执行完了再执行，造成死锁
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"回到主线程:%@",[NSThread currentThread]);
+         NSLog(2);
+    });
+    NSLog(@"1");
+     */
+    /* 第二种 while语句是死循环，所以造成死锁
+    while (1) {
+        NSLog(@"2");
+    }
+    NSLog(@"1");
+     */
+    /* 第三种
+     2是添加在串行队列中，所以2队列中只能有一个任务在同时执行，3会等待4执行完了再往下执行，4会等待3执行完了再往下执行
+     NSLog(@"1");
+     dispatch_queue_t queue = dispatch_queue_create("com.demo.serialQueue", DISPATCH_QUEUE_SERIAL);
+     dispatch_async(queue, ^{
+     NSLog(@"2");
+     dispatch_sync(queue, ^{
+     NSLog(@"3");
+     });
+     NSLog(@"4");
+     });
+     NSLog(@"5");
+     */
+    /*
+     
+     */
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            NSLog(@"1");
+        });
+    });
+    while (1) {
+        
+    }
+    NSLog(@"1");
 }
+
 
 
 - (void)didReceiveMemoryWarning {
